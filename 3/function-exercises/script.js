8d709370e79b025068f4c407cb40f75b55dd081c
Sartaj <sartaj@sartaj.me>
2014-02-12 15:25:58 -0600

    added function lessons

---
+++ 3/function-exercises/script.js
@@ -0,0 +1,136 @@
+/**
+ * Exercise 0: angryGandalf (example)
+**/
+
+// Define angryGandalf with input parameters
+var angryGandalf = function(type) {
+
+	// Define the variables needed
+	var message;
+
+	// Run conditional on type for 'grey' and 'white'
+	if (type === 'grey') {
+		message = "YOU...SHALL NOT... PASS!";
+	} else if (type === 'white') {
+		message = "You merely passed me to your demise."
+	} else {
+		message = "I don't think I'm Gandalf anymore."
+	}
+
+	// Return message
+	return message 
+
+}
+
+// console.log(angryGandalf('white')); // Expect "YOU...SHALL NOT... PASS!"
+// console.log(angryGandalf('grey')); // Expect "You merely passed me to your demise."
+// console.log(angryGandalf(2)); // Expect "I don't think I'm Gandalf anymore."
+
+
+
+
+/**
+ * Exercise 1: tellFortune
+**/
+
+// Create function with name tellFortune and appropriate parameters
+
+	// Define string variable that will have that output
+
+	// Return that string
+
+// console.log(tellFortune('bball player', 'spain', 'Shaq', 3)); // Expect 'You will be a bball player in spain and married to Shaq  with 3 kids. '
+// console.log(tellFortune('stunt double', 'Japan', 'Ryan Gosling', 3000)); // Expect 'You will be a stunt double in Japan and married to Ryan Gosling  with 3000 kids. '
+// console.log(tellFortune('Elvis impersonator', 'Russia', 'The Oatmeal', 0)); // You will be a Elvis impersonator in Russia and married to The Oatmeal  with 0 kids. 
+
+
+
+/**
+ * Exercise 2: calculateAge
+**/
+
+// Create function with name tellFortune and appropriate parameters
+
+	// Define string variable that will have that output
+    
+    // return string to pass the tests below
+
+// calculateAge(1984, 2012); // You are either 28 or 27 
+// calculateAge(1988, 2012); // You are either 24 or 23 
+// calculateAge(1982, 2012); // You are either 30 or 29 
+
+
+
+/**
+ * Exercise 3: calculateSupply
+**/
+
+// Create function with name tellFortune and appropriate parameters
+
+	// Define string variable
+    
+    // Do math to substract max string - string
+
+// calculateAge(1984, 2012); // You are either 28 or 27 
+// calculateAge(1988, 2012); // You are either 24 or 23 
+// calculateAge(1982, 2012); // You are either 30 or 29 
+
+
+
+/**
+ * Exercise 4: The Circle Calculator
+**/
+
+// Create function with name calcDiameterFromRadius and appropriate parameters
+
+	// Define string variable that will have that output
+    
+    // return string to pass the tests below
+
+// Create function with name calcCircumfrence and appropriate parameters
+
+	// Define string variable that will have that output
+
+	// Get Diameter from radius with calcDiameterFromRadius() made above
+    
+    // Create string to pass test below
+
+    // return string
+
+// calcCircumfrence(3); // The circumference is 28.26
+// calcCircumfrence(4); // The circumference is 50.24
+// calcCircumfrence(5); // The circumference is 78.5
+
+
+/**
+ * Exercise 5:  The Temperature Converter
+**/
+
+// Create function with name convertTemp and appropriate parameters
+
+	// Define string variable that will have that output
+    
+    // return string to pass the tests below
+
+// Create function with name convertTemp and appropriate parameters
+
+	// Define string variable that will have that output
+    	
+    // Write a conditional if then statement that returns 'boiling', 'liquid', or 'freezing' depending on what the temperature is.
+
+    // return string to pass the tests below
+
+// Create a function called `waterStateAt`
+
+	// Define string variable that will have that output
+    
+    // Save a new variable with "convertTemp"
+
+    // Write a conditional if then statement that returns 'boiling', 'liquid', or 'freezing' depending on what the temperature is.
+
+    // return string to pass the tests below
+
+
+// waterStatAt(50, inputType); // "It's current 50 Degrees in Celcius standard and water is liquid. "
+// waterStatAt(213, inputType); // "It's current 213 Degrees in Farenheit standard and water is boiling. "
+// waterStatAt(-1, inputType); // "It's current 11 Degrees in F standard and water is liquid. " 
